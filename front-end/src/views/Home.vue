f2ddb913528b46f905e6db8c4a7b2e619b765f56
sihnyoung <shersy716@gmail.com>
2020-03-28 21:34:15 -0600

    lab4_done?

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,12 +1,90 @@
 <template>
-<div class="home">
-</div>
+  <div class="home">
+    <section class="image-gallery">
+      <div class="image" v-for="item in items" :key="item.id">
+        <h2>{{item.title}}</h2>
+        <p>{{item.description}}</p>
+        <img :src="item.path" />
+      </div>
+    </section>
+    <footer>
+      <p>my git hub: <a href="https://github.com/BYU-CS-260-Winter-2020/lab-4-museum-of-ordinary-objects-spark58.git">
+      link</a>.</p>
+    </footer>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios';
 export default {
   name: 'Home',
+  data() {
+  return {
+   items: [],
+  }
+},
+created() {
+  this.getItems();
+},
+methods: {
+  async getItems() {
+    try {
+      let response = await axios.get("/api/items");
+      this.items = response.data;
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}
 }
 </script>
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+}
+
+/* Masonry */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+.image-gallery {
+  column-gap: 1.5em;
+}
+
+.image {
+  margin: 0 0 1.5em;
+  display: inline-block;
+  width: 100%;
+}
+
+.image img {
+  width: 100%;
+}
+
+/* Masonry on large screens */
+@media only screen and (min-width: 1024px) {
+  .image-gallery {
+    column-count: 4;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .image-gallery {
+    column-count: 3;
+  }
+}
+
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .image-gallery {
+    column-count: 2;
+  }
+}
+</style>
