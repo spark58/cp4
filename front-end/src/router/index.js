6c6a2c0ff0ededd4b30b9689b4172aaeaca6aba6
pfiguer2 <43146471+pfiguer2@users.noreply.github.com>
2020-04-13 21:24:11 -0600

    Update index.js

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Admin from '../views/Admin.vue'
+import YourRecipe from '../views/YourRecipe.vue'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,11 @@ const routes = [{
     path: '/admin',
     name: 'admin',
     component: Admin
+  },
+  {
+    path: '/yourrecipe',
+    name: 'yourrecipe',
+    component: YourRecipe
   }
 ]
 
